c57026b7960433c3e6e3189ed27b7a44dd815ed8
Basupally Karthik Reddy <basupallykarthikreddy@gmail.com>
2024-04-06 21:31:35 +0530

    practice

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,1 +1,7 @@
-console.log("Hello world !");
+let name = "Karthik Reddy";
+let age = 24;
+let money = 1000.5;
+console.log("Hello world from " + name);
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof money);
